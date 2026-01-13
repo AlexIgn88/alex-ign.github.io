527ad70fad16e6b0e7f4f9aaf3be9c1e832243d8
Игнатов Алексей <a.ignatov@vr-protect.ru>
2026-01-13 16:53:10 +0300

    fix: change http to https in API_BASE_URL

--- src/common/common-consts.ts
+++ src/common/common-consts.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://19429ba06ff2.vps.myjino.ru/api';
-
-export const TOKEN_STORAGE_KEY = 'auth_token';
+export const API_BASE_URL = 'https://19429ba06ff2.vps.myjino.ru/api';
 
 export const API = { SIGNUP: '/signup' };
+
+export const TOKEN_STORAGE_KEY = 'auth_token';
